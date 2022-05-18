8a462576c7424bffb1dce9e085b242535d37e77a
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-05-18 15:01:05 +0530

    string methods

--- javascript-demo.js
+++ javascript-demo.js
@@ -46,5 +46,70 @@ class Person {
 }
 var person = new Person("Monika");
 console.log("print name as", this.name);
-person.printNameFunction();
-person.printNameArrow();
+// person.printNameFunction();
+// person.printNameArrow();
+
+product = (num1 = 1, num2 = 1) => {
+  return num1 * num2;
+};
+
+let prodResult = product(10, 20);
+console.log("the result is ${prodResult}");
+console.log(`The result is ${prodResult}`);
+console.log('Hi My \n name is \n "Monika"');
+console.log(`Hi My
+ name is 
+ "Monika"`);
+
+//  string methods
+var stringOne = "This is a React session ";
+var stringTwo = "to learn Frontend and Backend";
+
+// 1) charAt => returns character at a specifoed location
+
+console.log(stringOne.charAt(1));
+
+// 2)charCodeAt() => return unicode od character at a specified location
+
+console.log(stringOne.charCodeAt(1));
+
+// 3)concat => join the strings
+console.log(stringOne.concat(stringTwo));
+
+// 4) fromCharCode()
+
+// 5)endsWith
+console.log(stringOne.endsWith("ssion "));
+
+// 6)includes
+console.log(stringTwo.includes("learn"));
+
+// indexOf
+console.log(stringTwo.indexOf("end"));
+
+// lastIndexOf
+console.log(stringTwo.lastIndexOf("end"));
+
+// replace
+console.log(stringTwo.replace(/end/g, "END"));
+
+// slice
+console.log(stringTwo.slice(9, 14));
+
+// split
+console.log(stringTwo.split("and"));
+
+// startWith
+console.log(stringTwo.startsWith("to"));
+
+// toLowercase
+console.log(stringOne.toLowerCase());
+
+// toUpperCase
+console.log(stringOne.toUpperCase());
+
+var name1 = "monika  abhijeet   ";
+var name2 = " monika abhijeet";
+
+// trim
+console.log(name1.trim() === name2.trim());

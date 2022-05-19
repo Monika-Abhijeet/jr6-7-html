0983009a6e77ab04b940822a3ae81b627086c412
Monika Abhijeet <monikaabhijeet@Monikas-MacBook-Pro.local>
2022-05-19 12:21:53 +0530

    typescript concepts

---
+++ Typescript/main.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+exports.message = void 0;
+exports.message = "Hello world";
+console.log(exports.message);
+console.log(8 + 2 * 5 - 3);
+console.log("i am in watch mode");
---
+++ Typescript/main.ts
@@ -0,0 +1,4 @@
+export let message = "Hello world";
+console.log(message);
+console.log(8 + 2 * 5 - 3);
+console.log("i am in watch mode");
